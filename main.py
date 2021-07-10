ca450b17c20253ebdca02469dd01f0460d768347
dagarciam <dagarciam@minsait.com>
2021-07-09 19:14:34 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
 
+from minsait.ttaa.datio.common.Constants import *
 from minsait.ttaa.datio.engine.Transformer import Transformer
 
 if __name__ == '__main__':
     spark: SparkSession = SparkSession \
         .builder \
-        .master(SPARK_) \
+        .master(SPARK_MODE) \
         .getOrCreate()
     transformer = Transformer(spark)
